a76c1f1db6285a8a2e542d96c55f0c02fa7d66f5
Marshall Demars <marshall.demars@mths.ca>
2022-09-18 00:30:21 +0000

    new file

---
+++ area_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars
+# Created on: Sept 2022
+# This program calculates area and perimeter with 5cm and 3cm dimensions
+
+
+def main():
+    # this function calculates the area and perimeter
+    print("If a rectangle has the dimensions:")
+    print("5cm and 3cm")
+    print("\nArea is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(3 + 5(*2)))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
